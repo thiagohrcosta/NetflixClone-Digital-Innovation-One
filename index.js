4cc5c37d81260b17cb427d49320dc31a4dfda7be
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-08-25 19:41:24 -0300

    Add gitignore, procfile and update ports

--- index.js
+++ index.js
@@ -63,6 +63,11 @@ app.get("/tvShowContent/:id", function(req, res){
   res.render("tvShowContent");
 });
 
-app.listen(3000, function(){
-  console.log("Server running on port 3000.")
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
+
+app.listen(port, function(){
+  console.log("Server has started successfully.")
 })
